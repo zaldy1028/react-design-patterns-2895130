5e048d43055fa7e6b7eef9444ba9b329983120db
Elizalde Esteban <zaldy1028@gmail.com>
2021-11-27 20:33:23 +0800

    progress on split screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { SplitScreen } from './SplitScreen';
 
+const LeftHandComponent = () => {
+  return <h1 style={{ backgroundColor: 'green' }}>Left!</h1>;
+}
+
+const RightHandComponent = () => {
+  return <h1 style={{ backgroundColor: 'red' }}>Right!</h1>;
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+    <SplitScreen
+     left={LeftHandComponent}
+     right={RightHandComponent}
+     leftWeight={1}
+     rightWeight={3}
+    />
     </div>
   );
 }
